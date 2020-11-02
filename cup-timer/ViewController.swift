4b245d3c2e8db8844c558c5aa8bc70526b7d7846
julesug@gmail.com <julesug@gmail.com>
2020-11-02 18:28:46 +0900

    first commit

--- cup-timer/ViewController.swift
+++ cup-timer/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet weak var timerText: UILabel!
+    
+    @IBAction func timeSelected(_ sender: UIButton) {
+        
+        
+    }
+    
+    
 }
 
